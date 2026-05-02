adc5d9318537371a0b0efa0a61020ef5bb354e32
Amanda Steeples <amandasteeples304@gmail.com>
2026-05-02 21:36:17 +0100

    Map new keys to break vercel cache

--- src/utils/supabase/client.js
+++ src/utils/supabase/client.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_NEW_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_NEW_SUPABASE_ANON_KEY;
 if (!supabaseUrl || !supabaseKey) {
   console.error("Supabase environment variables are missing");
 }
